b36319b8dbdc03779e0cd9ed5e6f7f353b350a28
jonwite8-max <jonwite8@gmail.com>
2026-09-07 01:07:08 +0100

    fix(system): wire health module into application root

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { domainModules } from './modules';
+import { SystemModule } from './system/system.module';
 
 @Module({
-  imports: [ThrottlerModule.forRoot([{ ttl: 60_000, limit: 120 }]), ...domainModules],
+  imports: [ThrottlerModule.forRoot([{ ttl: 60_000, limit: 120 }]), SystemModule, ...domainModules],
   providers: [{ provide: APP_GUARD, useClass: ThrottlerGuard }],
 })
 export class AppModule {}
